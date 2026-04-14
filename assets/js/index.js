70ebc54d6d67a2f48c22c72f2852f3b64ddd4f52
frontenddev <frontendev344@gmail.com>
2026-04-14 16:51:50 +0530

    fixes

--- assets/js/index.js
+++ assets/js/index.js
@@ -52,13 +52,3 @@ AOS.init({
 
 
 
-
-  window.addEventListener("load", function () {
-    const loader = document.getElementById("loader");
-    loader.style.opacity = "0";
-    loader.style.transition = "opacity 0.5s ease";
-
-    setTimeout(() => {
-      loader.style.display = "none";
-    }, 500);
-  });
