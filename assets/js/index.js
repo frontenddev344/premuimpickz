49f9c11dbdd77d9f1da45825804b0285531f29cd
frontenddev <frontendev344@gmail.com>
2026-04-14 16:46:59 +0530

    loader added

--- assets/js/index.js
+++ assets/js/index.js
@@ -50,3 +50,15 @@ AOS.init({
 });
 // AOS JS End
 
+
+
+
+  window.addEventListener("load", function () {
+    const loader = document.getElementById("loader");
+    loader.style.opacity = "0";
+    loader.style.transition = "opacity 0.5s ease";
+
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 500);
+  });
